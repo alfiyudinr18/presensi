78bd547213f1a35b364e372d12094bf749e1714d
alfiyudin46 <alfiyudin46@gmail.com>
2024-11-03 14:40:19 +0700

    cdn

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -27,7 +27,8 @@
     <link
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet" />
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css')}}" />
 
     <!-- Core CSS -->
