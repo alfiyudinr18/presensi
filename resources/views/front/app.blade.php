44f344c919cd12ab8e891164e6e46e7db5eb5b85
alfiyudin46 <alfiyudin46@gmail.com>
2024-11-10 10:24:29 +0700

    terbaru

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -12,6 +12,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('assets/frontend/css/styles.css')}}" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body>
